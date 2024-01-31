b3d4adc30c83540017856b84cbcffc00e69cf473
mygit@23 <naveenmadhesan23@gmail.com>
2024-01-31 14:10:35 +0530

    Add project files.

---
+++ WebAppPlayer/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPlayer.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        List<Player> ListPlayer = new List<Player>() { 
+        new Player(){Id=1,Name="MSD",Age=50,Team="Csk"},
+         new Player(){Id=2,Name="VK",Age=50,Team="RCB"},
+          new Player(){Id=3,Name="RS",Age=50,Team="MI"},
+           new Player(){Id=4,Name="Rizvi",Age=50,Team="Csk"},
+            new Player(){Id=5,Name="Rinku",Age=50,Team="KKR"},
+        };
+        public ActionResult Index()
+        {
+            return View(ListPlayer);
+        }
+    }
+}
---
+++ WebAppPlayer/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPlayer
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Team { get; set; }
+    }
+}
